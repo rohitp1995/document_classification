6c9420a65fcf2cbcfd2fbfed46ab4a9283d91c0f
rohit <rohit050695@gmail.com>
2022-01-01 02:21:19 +0530

    minor change

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-class dogcat:
+class predict:
     def __init__(self,filename):
         self.filename =filename
 
